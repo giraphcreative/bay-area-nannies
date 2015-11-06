776aabd8c39fbaae255acc01692294c21a752786
James Pederson <james@jpederson.com>
2015-11-05 18:01:39 -0600

    Remove unneeded javascript, and adjust code that controls sizes of content images.

--- js/src/main.js
+++ js/src/main.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function($){
 	var menu = $( 'header nav' ),
 		menu_toggle = menu.find( 'button.menu-toggle' ),
 		menu_ul = menu.find( '.nav-menu' ),
-		fluid_images = $( '.content-area img, .site-content img' ),
+		fluid_images = $( '.main-content img, .content-style img' ),
 		sidebar = $( '.sidebar' ),
 		large_title = $( '.large-title' ),
 		left_menu = $( '.sidebar .menu' ),
