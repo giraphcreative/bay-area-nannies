cdb30f2d455a063fa65bbd2b53e827eb263e5206
James Pederson <james@jpederson.com>
2015-11-22 13:45:13 -0600

    Update all but the store link in the homepage icons area.

--- page-front.php
+++ page-front.php
@@ -33,13 +33,13 @@ get_header();
 		<div class="home-links">
 			<div class="wrap">
 				<ul>
-					<li><a href="#"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-books.png"> Books 4 Babes</a></li>
-					<li><a href="#"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-places.png"> Places to Go</a></li>
-					<li><a href="#"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-teach-talk.png"> Teach Baby to Talk</a></li>
-					<li><a href="#"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-music.png"> Music for Kids</a></li>
-					<li><a href="#"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-separation.png"> Separation Strategies</a></li>
-					<li><a href="#"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-needs.png"> Special Needs</a></li>
-					<li><a href="#"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-toys.png"> Best Toys</a></li>
+					<li><a href="/2015/11/22/books-4-babes/"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-books.png"> Books 4 Babes</a></li>
+					<li><a href="/2015/11/22/places-to-go/"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-places.png"> Places to Go</a></li>
+					<li><a href="/2015/11/22/teach-baby-to-talk/"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-teach-talk.png"> Teach Baby to Talk</a></li>
+					<li><a href="/2015/11/22/great-music-for-kids/"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-music.png"> Music for Kids</a></li>
+					<li><a href="/2015/11/22/separation-strategies/"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-separation.png"> Separation Strategies</a></li>
+					<li><a href="/2015/11/22/special-needs/"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-needs.png"> Special Needs</a></li>
+					<li><a href="/2015/11/22/best-toys/"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-toys.png"> Best Toys</a></li>
 					<li><a href="#"><img src="<?php bloginfo( 'template_url' ) ?>/img/home-store.png"> Baby Store</a></li>
 				</ul>
 			</div>
