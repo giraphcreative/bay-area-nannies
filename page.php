f31fec04ca87bbea07d55d95b54297ee57da6eec
James Pederson <james@jpederson.com>
2015-11-30 14:44:06 -0600

    Add title to the main page template.

--- page.php
+++ page.php
@@ -18,7 +18,10 @@ if ( empty( $color ) ) $color = 'forest';
 			<?php 
 			// do post loop
 			if ( have_posts() ) :
-				while ( have_posts() ) : the_post(); 
+				while ( have_posts() ) : the_post();
+					?>
+					<h1><?php the_title(); ?></h1>
+					<?php
 					the_content();
 				endwhile;
 			endif;
