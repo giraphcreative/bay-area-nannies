08328c259fc3ea01dbc479dab856372cb93bff2a
James Pederson <james@jpederson.com>
2015-12-22 17:46:42 -0600

    Adjustments to social icons in header, and text/icons in footer.

--- footer.php
+++ footer.php
@@ -17,9 +17,16 @@
 
 <footer class="footer">
 	<div class="wrap">
-		&copy; Bay Area Super Nanny 2015<br>
-		Nurturing & Loving Nanny/Doula with over 21yrs of experience!<br>
-		<a href="mailto:bayarea1nanny@yahoo.com">Bayarea1nanny@yahoo.com</a>
+		<p>&copy; Bay Area Super Nanny&reg; 2015. All Rights Reserved.<br>
+			Loving, Nurturing Nannies, Mannies, Postpartum Doulas, Caregivers</p>
+		<p><a href="mailto:bayarea1nanny@yahoo.com">Bayarea1nanny@yahoo.com</a></p>
+		<p>
+			Follow Us:<br>
+			<a href="https://www.instagram.com/bayareasupernanny/"><img src="<?php bloginfo( "template_url" ) ?>/img/social-instagram.png"></a>
+			<a href="https://www.youtube.com/channel/UCHGXqUmow_vSFnlY4QF67xw/feed"><img src="<?php bloginfo( "template_url" ) ?>/img/social-youtube.png"></a>
+			<a href="https://www.linkedin.com/in/cherrelle-williams-59561250"><img src="<?php bloginfo( "template_url" ) ?>/img/social-linkedin.png"></a>
+			<a href="https://m.facebook.com/profile.php?id=279802892099777&tsid=0.10620086104609072&source=typeahead"><img src="<?php bloginfo( "template_url" ) ?>/img/social-facebook.png"></a>
+		</p>
 	</div>
 </footer><!-- #colophon -->
 
